fc2af588eb4dd1a3ec6a392a96c4dc5636e5d3b1
JasonEvan <7ason3van@gmail.com>
2025-02-19 22:30:28 +0700

    Optimizing background on safari

--- components/main-background.tsx
+++ components/main-background.tsx
@@ -4,10 +4,11 @@ export default function Background({
   children?: React.ReactNode;
 }) {
   return (
-    <div
-      id="background-main"
-      className="relative w-full h-full bg-cover bg-center bg-fixed"
-    >
+    <div className="relative w-full h-full">
+      <div
+        id="background-main"
+        className="fixed top-0 left-0 w-full h-screen bg-cover bg-center z-[-1]"
+      />
       {children}
     </div>
   );
