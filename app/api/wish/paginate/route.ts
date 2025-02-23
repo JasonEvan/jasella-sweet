958cb578d617b85b31e29454f3fef2af0c5487a7
JasonEvan <7ason3van@gmail.com>
2025-02-23 13:17:18 +0700

    Add new picture and fixing bug

--- app/api/wish/paginate/route.ts
+++ app/api/wish/paginate/route.ts
@@ -18,15 +18,6 @@ export async function GET(request: NextRequest) {
     cache: "no-store",
   });
 
-  if (!res.ok) {
-    return NextResponse.json(
-      {
-        message: "",
-      },
-      { status: res.status }
-    );
-  }
-
   const { data: count } = await res.json();
   const totalPage = Math.ceil(count / 5.0);
   if (activePage > totalPage) {
